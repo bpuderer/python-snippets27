3cf2c3d495fac51765e314c02c2db8b03c3841f2
Ben Puderer <bpuderer@yahoo.com>
2016-03-04 22:08:47 -0500

    another csv read example

--- csv_qs/csv_qs.py
+++ csv_qs/csv_qs.py
@@ -1,5 +1,21 @@
 import csv
 
+
+with open('guitars.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row
+
+print "-----"
+
+with open('guitars.csv', 'rb') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        print row
+
+print "-----"
+
+
 csv.register_dialect('albums', escapechar='\\', quoting=csv.QUOTE_NONE)
 
 with open('rock.csv', 'rb') as f:
@@ -14,7 +30,7 @@ with open('rock.csv', 'rb') as f:
     for row in reader:
         print row
 
-rows = [("Rainbow", "Long Live Rock 'n' Roll", 1978),("Black Sabbath","Dehumanizer", 1992), ("Helloween", "Keeper of the Seven Keys, Pt. 2", 1988)]
+rows = [("Rainbow", "Long Live Rock 'n' Roll", 1978),("Black Sabbath", "Dehumanizer", 1992), ("Helloween", "Keeper of the Seven Keys, Pt. 2", 1988)]
 with open('rock_py.csv', 'wb') as f:
     writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
     writer.writerows(rows)
