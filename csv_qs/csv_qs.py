1e6a506e57a2cf6451898b75671d87a304550177
Ben Puderer <bpuderer@yahoo.com>
2016-03-06 11:52:14 -0500

    more detail in csv qs

--- csv_qs/csv_qs.py
+++ csv_qs/csv_qs.py
@@ -1,6 +1,8 @@
 import csv
 
 
+# reading
+
 with open('guitars.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -9,12 +11,16 @@ with open('guitars.csv', 'rb') as f:
 print "-----"
 
 with open('guitars.csv', 'rb') as f:
+    # values in first row used if fieldnames parameter omitted
     reader = csv.DictReader(f)
     for row in reader:
         print row
 
 print "-----"
 
+
+# reading with a dialect - https://docs.python.org/2/library/csv.html#csv-fmt-params
+# reader, DictReader, writer, DictWriter default to csv.excel
 
 csv.register_dialect('albums', escapechar='\\', quoting=csv.QUOTE_NONE)
 
@@ -26,11 +32,27 @@ with open('rock.csv', 'rb') as f:
 print "-----"
 
 with open('rock.csv', 'rb') as f:
-    reader = csv.DictReader(f, dialect='albums')
+    field_names = ["Artist", "Album", "Year"]
+    reader = csv.DictReader(f, fieldnames=field_names, dialect='albums')
     for row in reader:
         print row
 
-rows = [("Rainbow", "Long Live Rock 'n' Roll", 1978),("Black Sabbath", "Dehumanizer", 1992), ("Helloween", "Keeper of the Seven Keys, Pt. 2", 1988)]
-with open('rock_py.csv', 'wb') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+
+# writing
+
+rows = [("Rainbow", "Long Live Rock 'n' Roll", 1978), ("Black Sabbath", "Dehumanizer", 1992), ("Helloween", "Keeper of the Seven Keys, Pt. 2", 1988), ("David Bowie", '"Heroes"', 1977)]
+with open('rock_out.csv', 'wb') as f:
+    print "wrote file rock_out.csv"
+    writer = csv.writer(f)
     writer.writerows(rows)
+
+with open('guitars_out.csv', 'wb') as f:
+    print "wrote file guitars_out.csv"
+    # fieldnames defines the order fields are written since dictionaries are not ordered
+    # all dictionary keys must be present in fieldnames (ValueError) but not vice versa
+    field_names = ["Make", "Model", "Finish"]
+    writer = csv.DictWriter(f, fieldnames=field_names)
+    writer.writeheader()
+    writer.writerow({'Make': "Gibson", 'Model': "Les Paul Custom", 'Finish': "Black"})
+    writer.writerow({'Make': "Ibanez", 'Finish': "Desert Red", 'Model': "RT150"})
+    writer.writerow({'Model': "CE", 'Finish': "Vintage Sunburst", 'Make': "PRS"})
