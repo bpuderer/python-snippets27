c8d24647ad89684d89dd057cd21a594ec6076b75
Ben Puderer <bpuderer@yahoo.com>
2016-04-10 08:50:14 -0400

    truthiness

---
+++ truthiness.py
@@ -0,0 +1,26 @@
+if not 0:
+    print "zero value ints are False"
+
+if not 0.0:
+    print "zero value floats are False"
+
+if not None:
+    print "None is False. Use is/is not to check for None."
+
+if not False:
+    print "False is False"
+
+if not '':
+    print "empty strings are False"
+
+if not []:
+    print "empty lists are False"
+
+if not ():
+    print "empty tuples are False"
+
+if not {}:
+    print "dicts with no keys are False"
+
+if not set([]):
+    print "empty sets are False"
