8b7e546b4398d1bfd86f216b3849f501a942c18a
Ben Puderer <bpuderer@yahoo.com>
2016-07-22 21:39:32 -0400

    more classes

--- general/classes.py
+++ general/classes.py
@@ -1,38 +1,59 @@
+"""classes demo"""
+
 class MyClass(object):
-    """MyClass docstring"""
+    """MyClass docstring.  inherit from object = new style object"""
 
-    #class variable. immutable.
-    i = 123
+    #class variable. use immutables.
+    version = '0.1'
 
-    #https://docs.python.org/2.7/tutorial/classes.html#private-variables-and-class-local-references
-    #convention to denote private / implementation detail:
-    #start name with an underscore
-
-    #use name mangling to prevent name clashes when subclassing
-    #https://www.youtube.com/watch?v=HTLu2DFOdTg&t=33m25s
-    #RH- "one of my problems with a variable named self is it implies that it's you.
-    #but it's not you.  it is you or your children."
-    #RH- "making sure that self actually refers to you"
-    #RH- "...all about freedom.  It makes your subclasses free to override any one method
-    #without breaking the others."
+    #watch https://www.youtube.com/watch?v=HTLu2DFOdTg
+    #great presentation from Raymond Hettinger
+    #docstrings below quote and paraphrase RH
+    #if good, useful, correct -> RH
+    #if not -> misquote, me
 
     def __init__(self, val):
+        """dunder (double underscore) init is *not* a constructor.
+
+        self is the instance that has already been made when init is called.
+        it is an initializer that populates self"""
         self.lst = []
         self.val = val
-        print self.__no_clash()
+        print self.__no_clash()    #demo prevention of name clash
 
     def add(self, x):
+        """add x to lst"""
         self.lst.append(x)
 
     def double_add(self, x):
+        """add x twice"""
         self.add(x)
         self.add(x)
+
+    @staticmethod
+    def func_in_class():
+        """purpose of static method is to attach functions to classes
+
+        because that's where people look for it"""
+        return "just a function hanging out in a class"
 
     def no_clash(self):
-        return 'no_clash from MyClass'
+        """demo name mangling
+
+        name mangling used to prevent name clashes with subclassing.
+        """
+        return self.val * 1.0
+
+    @classmethod
+    def fromtripleval(cls, tval):
+        """alternative constructor
+
+        https://www.youtube.com/watch?v=HTLu2DFOdTg&t=23m28s"""
+        val = tval / 3
+        return cls(val)
 
     def __str__(self):
-        return "val:"+str(self.val)+" lst:"+str(self.lst)+" i:"+str(self.i)
+        return "val:"+str(self.val)+" lst:"+str(self.lst)+" ver:"+self.version
 
     def __add__(self, other):
         tmp = MyClass(self.val + other.val)
@@ -43,27 +64,45 @@ class MyClass(object):
 
 
 class MyClass2(MyClass):
+    """MyClass2 docstring"""
+
     def double_add(self, x):
+        """override method. add two times x to lst, not x twice"""
         self.add(x*2)
 
     def no_clash(self):
-        return 'no_clash from MyClass2'
+        """override no_clash. demo name mangling.  return 110% of val"""
+        return self.val * 1.1
 
 
 a = MyClass(42)
 print a.no_clash()
+print a.func_in_class()
 a.add(2112)
 a.double_add(2010)
 print a
 
 print '--'
 
-b = MyClass2(10)
+#b uses alt constructor
+b = MyClass.fromtripleval(27)
 print b.no_clash()
-b.add(2001)
+print b.func_in_class()
+b.add(2112)
 b.double_add(2010)
 print b
 
 print '--'
 
+#c is a MyClass2 not MyClass
+c = MyClass2(10)
+print c.no_clash()
+print c.func_in_class()
+c.add(2001)
+c.double_add(2010)
+print c
+
+print '--'
+
 print a + b
+print b + c
