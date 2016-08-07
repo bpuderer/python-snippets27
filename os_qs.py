63c37c87207733eea537897baf1f1e6ad0344330
Ben Puderer <bpuderer@yahoo.com>
2016-08-07 12:19:59 -0400

    os.listdir, url with params, urllib

--- os_qs.py
+++ os_qs.py
@@ -1,6 +1,7 @@
 import fnmatch
 import os
 import platform
+import shlex
 import socket
 import time
 
@@ -21,14 +22,23 @@ os.environ['PYTEST'] = 'set from py script'
 print os.getenv('PYTEST', 'default val')
 
 
+dirs = [d for d in os.listdir(os.curdir) if os.path.isdir(d)]
+print "dirs in current dir:", dirs
+
+
 # recursively walk directory searching for files
 # with Unix filename pattern matching
 matches = []
-for root, dirnames, filenames in os.walk('.'):
+for dirpath, _, filenames in os.walk(os.curdir):
     for filename in fnmatch.filter(filenames, '*.csv'):
-        matches.append(os.path.join(root, filename))
+        matches.append(os.path.join(dirpath, filename))
 print 'csv files from here on down:', matches
 
 
 print "last modified time:", time.ctime(os.path.getmtime(__file__))
 print "ctime:", time.ctime(os.path.getctime(__file__))
+
+
+#split string using shell-like syntax
+#useful with subprocess.Popen args argument
+print shlex.split("tail -n20 somefile.log | grep -i spam")
--- urlparse_qs.py
+++ urlparse_qs.py
@@ -1,19 +1,38 @@
-from urlparse import urlparse, parse_qs
+import urllib
+from urlparse import parse_qs, urlparse
 
-url = "http://user:password@host:80/pathseg1/pathseg2?field1=value1&field2=value2#fragment"
 
+# https://tools.ietf.org/html/rfc3986
+# http://doriantaylor.com/policy/http-url-path-parameter-syntax
+
+url = "http://user:password@host:80/pathseg1/pathseg2;param1;param2=v1,v2?field1=value1&field2=value2#fragment"
 print url
+
 parsed_url = urlparse(url)
+print parsed_url
+
 print "scheme:", parsed_url.scheme
 print "netloc:", parsed_url.netloc
 print "path:", parsed_url.path
+print "params:", parsed_url.params
 print "query:", parsed_url.query
 print "fragment:", parsed_url.fragment
 print "username:", parsed_url.username
 print "password:", parsed_url.password
 print "hostname:", parsed_url.hostname
 print "port:", parsed_url.port
-print "type of port:", type(parsed_url.port)
 
 qs = parsed_url.query
+# & or ; separator
 print "parsed query string:", parse_qs(qs)
+
+
+# build query string
+print urllib.urlencode({'foo': 0, 'bar': 1})
+
+
+# https://www.ietf.org/rfc/rfc1738.txt
+p = "/15% off/Lenny&Squiggy"
+q = urllib.quote(p)
+print q
+print urllib.unquote(q)
