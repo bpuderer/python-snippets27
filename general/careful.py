ef3da7b2cc1363b1675b0778c3a4493117c32cbc
Ben Puderer <bpuderer@yahoo.com>
2016-12-01 23:04:44 -0500

    couple more careful ex

--- general/careful.py
+++ general/careful.py
@@ -2,27 +2,28 @@
 import time
 
 
-
 # mutable default args
-def append_to(element, alist=[]):
-    # default args are only evaluated when defined, not
-    # each time it is called
-    alist.append(element)
-    return alist
+def append_to(element, lst=[]):
+    # default args are only evaluated when defined, not each call
+    lst.append(element)
+    return lst
 
 print append_to(42)
 print "???", append_to(2112)
 
+print '-'
+
 # typical solution
-def append_to2(element, alist=None):
-    if alist is None:
-        alist = []
-    alist.append(element)
-    return alist
+def append_to_fixed(element, lst=None):
+    if lst is None:
+        lst = []
+    lst.append(element)
+    return lst
 
-print append_to2(42)
-print append_to2(2112)
+print append_to_fixed(42)
+print append_to_fixed(2112)
 
+print '-'
 
 # another demo of default args only evaluated when defined
 print "current time:", time.time()
@@ -37,6 +38,25 @@ print_time()
 time.sleep(1)
 print_time()
 
+
+print '---'
+
+
+# don't use mutable class variables
+class Foo(object):
+    bar = []
+
+    def add(self, baz):
+        self.bar.append(baz)
+
+f1 = Foo()
+f2 = Foo()
+f1.add('spam')
+print 'f1.bar:', f1.bar
+print 'f2.bar:', f2.bar, '???'
+
+
+print '---'
 
 
 # global statement
@@ -69,6 +89,8 @@ foo3()
 print baz
 
 
+print '---'
+
 
 # one item tuples need a trailing comma
 a = (3)
@@ -76,6 +98,8 @@ print a, type(a)
 b = (3,)
 print b, type(b)
 
+
+print '---'
 
 
 # multiple exceptions for a single except clause
@@ -85,6 +109,8 @@ try:
 except (AssertionError, ValueError) as e:
     print e
 
+
+print '---'
 
 
 # careful with + and lists
@@ -98,6 +124,8 @@ a += [6]
 a = a + [7]
 print "a:", a, "b:", b
 
+
+print '---'
 
 
 # init 2-D matrix
@@ -113,9 +141,27 @@ board[0][0] = 42
 print board
 
 
+print '---'
+
 
 # careful with mutables and chained assignment
 x = y = []
-x.append(42)
+x += [18]
+x.append(22)
 print x
 print y, 'uh oh'
+
+
+print '---'
+
+
+# https://docs.python.org/2/reference/expressions.html#generator-expressions
+# https://www.python.org/dev/peps/pep-0289/#early-binding-versus-late-binding
+# "the first (outermost) for-expression should be evaluated immediately and that the remaining
+# expressions be evaluated when the generator is executed."
+lst1 = [0, 1]
+lst2 = [10, 20]
+g = (i+j for i in lst1 for j in lst2)
+lst1 = [1, 2]
+lst2 = [100, 200]
+print g.next()
