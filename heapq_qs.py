c1e9063beaec93c523fb65a4d2d8caa63447d64a
Ben Puderer <bpuderer@yahoo.com>
2017-08-12 12:16:51 -0400

    fixed usage of heapq.merge

--- heapq_qs.py
+++ heapq_qs.py
@@ -2,20 +2,22 @@ import heapq
 import operator
 
 
-#heapq.nlargest(n, iterable[, key])
-#heapq.nsmallest(n, iterable[, key])
+# heapq.nlargest(n, iterable[, key])
+# heapq.nsmallest(n, iterable[, key])
 lst = [1, 2001, 42, 9, 2010, 2112, 7, 1776]
 print heapq.nlargest(3, lst)
 print heapq.nsmallest(2, lst)
 
-#with key
+# with key
 lst = [{'id': 0, 'val': 9}, {'id': 1, 'val': 2112}, {'id': 2, 'val': 2001},
        {'id': 3, 'val': 2010}, {'id': 4, 'val': 42}, {'id': 5, 'val': 13}]
 print heapq.nlargest(2, lst, key=operator.itemgetter('val'))
 
 
-#heapq.merge(*iterables)
-#docs: "Similar to sorted(itertools.chain(*iterables)) but returns an iterable"
-lst_a = [13, 1776, 42]
+# heapq.merge(*iterables)
+# merge multiple sorted iterables. returns iterator.
+# docs: "Similar to sorted(itertools.chain(*iterables)) but returns an iterable"
+lst_a = [13, 42, 1776]
 lst_b = [-10, 9, 2001]
-print list(heapq.merge(lst_a, lst_b))
+lst_c = [3, 28, 51]
+print list(heapq.merge(lst_a, lst_b, lst_c))
