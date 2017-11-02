05b57b6190c9e3eec0e88339ba63b41bc8f9bffc
Ben Puderer <bpuderer@yahoo.com>
2017-11-01 20:34:41 -0400

    and-or

--- general/conditionals.py
+++ general/conditionals.py
@@ -33,3 +33,16 @@ elif x == 0:
     print "is 0"
 else:
     print "is positive"
+
+
+print '---'
+
+
+# https://docs.python.org/2/reference/expressions.html#boolean-operations
+# https://stackoverflow.com/a/17794972
+# and/or return last evaluated argument
+print '' or 'default'
+print '' and 'default'
+
+print 'val' or 'default'
+print 'val' and 'default'
