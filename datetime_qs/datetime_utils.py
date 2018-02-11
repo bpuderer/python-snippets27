1fc4f7ccd11230379569c15c6f4e6404389be70e
Ben Puderer <bpuderer@yahoo.com>
2018-02-11 10:58:46 -0500

    in range

--- datetime_qs/datetime_utils.py
+++ datetime_qs/datetime_utils.py
@@ -9,14 +9,24 @@ from itertools import combinations
 Range = namedtuple('Range', ['start', 'end'])
 
 def overlap_days(r1, r2):
+    """number of overlap days of two ranges"""
     latest_start = max(r1.start, r2.start)
     earliest_end = min(r1.end, r2.end)
     delta = (earliest_end - latest_start).days + 1
     overlap = max(0, delta)
     return overlap
 
-def ranges_overlap(seq):
-    for r1, r2 in combinations(seq, 2):
+def ranges_overlap(ranges):
+    """check if ranges overlap with each other"""
+    for r1, r2 in combinations(ranges, 2):
         if overlap_days(r1, r2):
             return True
     return False
+
+def in_range(r1, r2):
+    """check if r1 is in the range of r2"""
+    return r1.start >= r2.start and r1.end <= r2.end
+
+def all_in_range(ranges, r2):
+    """check if all ranges are in range of r2"""
+    return all(in_range(r1, r2) for r1 in ranges)
--- datetime_qs/test_datetime_utils.py
+++ datetime_qs/test_datetime_utils.py
@@ -1,6 +1,6 @@
 import unittest
 from datetime import datetime
-from datetime_utils import overlap_days, ranges_overlap, Range
+from datetime_utils import Range, overlap_days, ranges_overlap, in_range, all_in_range
 
 
 class OverlapDaysTestCase(unittest.TestCase):
@@ -55,6 +55,54 @@ class OverlapDaysTestCase(unittest.TestCase):
         r5 = Range(start=datetime(2018, 1, 15), end=datetime(2018, 1, 19))
         self.assertTrue(ranges_overlap([r1, r2, r3, r4, r5]))
 
+
+class InRangeTestCase(unittest.TestCase):
+
+    def test_in_range_outside(self):
+        r1 = Range(start=datetime(2018, 1, 11), end=datetime(2018, 1, 13))
+        r2_1 = Range(start=datetime(2018, 1, 1), end=datetime(2018, 1, 5))
+        r2_2 = Range(start=datetime(2018, 1, 15), end=datetime(2018, 1, 20))
+        self.assertFalse(in_range(r1, r2_1))
+        self.assertFalse(in_range(r1, r2_2))
+
+    def test_in_range_some_overlap(self):
+        r1 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 15))
+        r2_1 = Range(start=datetime(2018, 1, 1), end=datetime(2018, 1, 10))
+        r2_2 = Range(start=datetime(2018, 1, 15), end=datetime(2018, 1, 20))
+        r2_3 = Range(start=datetime(2018, 1, 5), end=datetime(2018, 1, 14))
+        r2_4 = Range(start=datetime(2018, 1, 12), end=datetime(2018, 1, 20))
+        r2_5 = Range(start=datetime(2018, 1, 12), end=datetime(2018, 1, 14))
+        self.assertFalse(in_range(r1, r2_1))
+        self.assertFalse(in_range(r1, r2_2))
+        self.assertFalse(in_range(r1, r2_3))
+        self.assertFalse(in_range(r1, r2_4))
+        self.assertFalse(in_range(r1, r2_5))
+
+    def test_in_range(self):
+        r1 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 15))
+        r2_1 = Range(start=datetime(2018, 1, 5), end=datetime(2018, 1, 20))
+        r2_2 = Range(start=datetime(2018, 1, 5), end=datetime(2018, 1, 15))
+        r2_3 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 20))
+        self.assertTrue(in_range(r1, r1))
+        self.assertTrue(in_range(r1, r2_1))
+        self.assertTrue(in_range(r1, r2_2))
+        self.assertTrue(in_range(r1, r2_3))
+
+    def test_all_in_range(self):
+        r1_1 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 15))
+        r1_2 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 12))
+        r1_3 = Range(start=datetime(2018, 1, 12), end=datetime(2018, 1, 15))
+        r1_4 = Range(start=datetime(2018, 1, 11), end=datetime(2018, 1, 14))
+        r2 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 15))
+        self.assertTrue(all_in_range([r1_1, r1_2, r1_3, r1_4], r2))
+
+    def test_not_all_in_range(self):
+        r1_1 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 15))
+        r1_2 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 12))
+        r1_3 = Range(start=datetime(2018, 1, 12), end=datetime(2018, 1, 16))
+        r1_4 = Range(start=datetime(2018, 1, 11), end=datetime(2018, 1, 14))
+        r2 = Range(start=datetime(2018, 1, 10), end=datetime(2018, 1, 15))
+        self.assertFalse(all_in_range([r1_1, r1_2, r1_3, r1_4], r2))
 
 if __name__ == '__main__':
     unittest.main()
