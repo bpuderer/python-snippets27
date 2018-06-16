41f22280c685dbd33bb9d8c7e6fa2c27142da3d6
Ben Puderer <bpuderer@yahoo.com>
2018-06-16 16:52:44 -0400

    date instead of datetime as time shouldnt matter, replaced calc with lookup

--- datetime_qs/broadcast_cal_utils.py
+++ datetime_qs/broadcast_cal_utils.py
@@ -3,27 +3,33 @@
    https://en.wikipedia.org/wiki/Broadcast_calendar
 """
 
-import datetime
+from datetime import date, timedelta
 
 
 def broadcast_month_start(year, month):
-    """return start of broadcast month. always on monday"""
-    start_date = datetime.datetime(year, month, 1)
+    """return start date of broadcast month. always on monday"""
+    start_date = date(year, month, 1)
     while start_date.weekday() != 0:
-        start_date -= datetime.timedelta(days=1)
+        start_date -= timedelta(days=1)
     return start_date
 
 def quarter_start_month(quarter):
     """month that starts quarter for Gregorian calendar: Jan, Apr, Jul, Oct"""
     if quarter not in range(1, 5):
         raise ValueError("invalid quarter")
-    return 3 * quarter - 2
+    return {1: 1, 2: 4, 3: 7, 4: 10}[quarter]
+    # return 3 * quarter - 2
 
 def get_quarter(month):
     """return quarter for month"""
     if month not in range(1, 13):
         raise ValueError("invalid month")
-    return (month + 2) // 3
+    d = {1: 1, 2: 1, 3: 1,
+         4: 2, 5: 2, 6: 2,
+         7: 3, 8: 3, 9: 3,
+         10: 4, 11: 4, 12: 4}
+    return d[month]
+    # return (month + 2) // 3
 
 def next_quarter(year, quarter):
     """return next quarter for Gregorian calendar"""
@@ -34,39 +40,39 @@ def next_quarter(year, quarter):
     else:
         return year, quarter+1
 
-def broadcast_quarter_dates(year, quarter, num=1):
+def broadcast_quarter_dates(year, quarter, length=1):
     """start and end dates for broadcast quarter"""
     month = quarter_start_month(quarter)
     start = broadcast_month_start(year, month)
 
     # calculate end of quarter based on start of subsequent quarter
-    for _ in range(num):
+    for _ in range(length):
         year, quarter = next_quarter(year, quarter)
-    month = quarter_start_month(quarter) #maybe next_quarter returns month?
-    end = broadcast_month_start(year, month) - datetime.timedelta(days=1)
+    month = quarter_start_month(quarter)
+    end = broadcast_month_start(year, month) - timedelta(days=1)
 
     return start, end
 
-def next_quarter_broadcast_dates(num=1, start_offset=0, end_offset=0):
+def next_quarter_broadcast_dates(length=1, start_offset=0, end_offset=0):
     """return dates for next broadcast quarter (from now)"""
-    now = datetime.datetime.today()
+    now = date.today()
     year = now.year
     quarter = get_quarter(now.month)
 
     while True:
-        start, end = broadcast_quarter_dates(year, quarter, num)
+        start, end = broadcast_quarter_dates(year, quarter, length)
         if now < start:
             break
         year, quarter = next_quarter(year, quarter)
 
-    return (start+datetime.timedelta(days=start_offset),
-            end+datetime.timedelta(days=end_offset))
+    return (start+timedelta(days=start_offset),
+            end+timedelta(days=end_offset))
 
 def next_monday():
-    """return datetime for next monday"""
-    temp = datetime.datetime.today()
+    """return date for next monday"""
+    temp = date.today()
     # *next* monday, don't return today if it's monday
     while True:
-        temp += datetime.timedelta(days=1)
+        temp += timedelta(days=1)
         if temp.weekday() == 0:
             return temp
